fa12c07506de8c953805f87aa5432058022571f0
jahadtariq <muhammadjahadt@gmail.com>
2024-05-15 05:40:46 +0500

    implemeted basic layout of root pages and added assets to project

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,12 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 import "../globals.css";
 
+
+import Topbar from "@/components/shared/Topbar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +25,17 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          {children}
+          <Topbar/>
+          <main>
+            <LeftSidebar/>
+            <section className="main-container">
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
+            </section>
+            <RightSidebar/>
+          </main>
+          <Bottombar/>
         </body>
       </html>
     </ClerkProvider>
