68489752f567cee633a8d985d33901940e512f39
jahadtariq <muhammadjahadt@gmail.com>
2024-05-18 17:05:08 +0500

    initialized create-thread page

---
+++ app/(root)/create-thread/page.tsx
@@ -0,0 +1,14 @@
+import { currentUser } from "@clerk/nextjs/server";
+
+async function Page() {
+
+    const user = await currentUser();
+
+    if(!user) return null;
+
+    // const userInfo = await fetchUser(user.id);
+
+    return <h1 className="head-text">Create Thread</h1>
+};
+
+export default Page;
